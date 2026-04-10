8686ceedd8531527677ad5ae98fd6bf2463efdfd
savitri <savitrigamit123456@gmail.com>
2026-04-10 17:47:01 +0530

    updated Committee page

--- frontend/src/pages/committee/Committee.jsx
+++ frontend/src/pages/committee/Committee.jsx
@@ -88,7 +88,7 @@ const Committee = () => {
     </div>
      <div className="p-6">
         <h1 className="text-3xl font-bold text-center mb-6">
-        Library Committee
+        Library staff
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
