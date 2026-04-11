2e97c7c7fcbb32a5e6dc5e6e4c711e78bf62807a
snehakhokhar <khokharsneha8@gmail.com>
2026-04-11 15:03:05 +0530

    Added NPTEL and ONOS courses pages with UI and links

--- frontend/src/pages/courses/CourseOnos.jsx
+++ frontend/src/pages/courses/CourseOnos.jsx
@@ -5,29 +5,22 @@ const CourseOnos = () => {
 
     const courses = [
         {
-            name: "IEEE SDN eLearning Modules",
-            link: "https://sdn.ieee.org/education/elearning.html"
+            name: "Access Research Journals",
+            link: "https://www.onos.gov.in"
         },
         {
-            name: "Software Defined Networking with ONOS - YouTube",
-            link: "https://www.youtube.com/watch?v=iU9oNFKWkrU"
+            name: "Scientific Papers",
+           link: "https://www.onos.gov.in"
         },
         {
-            name: "How SDN will shape Networking - ONOS Project - YouTube",
-            link: "https://www.youtube.com/watch?v=c9-K5O_qYgA"
+            name: "Webinars & Events",
+            link: "https://www.onos.gov.in"
         },
         {
-            name: "Introduction to Network Function Virtualization (NFV) - ONOS Project - YouTube",
-            link: "https://www.youtube.com/watch?v=uoSO12hyuQw"
+            name: "Academic Publications",
+            link: "https://www.onos.gov.in"
         },
-        {
-            name: "Introduction to Software Defined Networking - Part 1 -youtube",
-            link: "https://www.youtube.com/watch?v=iU9oNFKWkrU"
-        },
-        {
-            name: "Introduction to Software Defined Networking - Part 2 -youtube",
-            link: "https://www.youtube.com/watch?v=DMv8TJv0-DQ"
-        },
+        
     ];
 
     return (
@@ -59,7 +52,7 @@ const CourseOnos = () => {
             </div>
              <div className="flex justify-center mt-12">
         <a
-          href="https://onosproject.org/"
+          href="https://www.onos.gov.in/"
           target="_blank"
           rel="noopener noreferrer"
           className="bg-gradient-to-r from-purple-500 to-indigo-600 text-white px-8 py-3 rounded-full shadow-lg hover:scale-105 transition"
